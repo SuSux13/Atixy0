09a2f8b2efe9d46325034b817c95f5b304783715
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-21 12:59:54 +0100

    Create UbuT.sh

---
+++ UbuT.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/rapid7/metasploit-framework
+cd metasploit-framework
+apt install sudo
+sudo apt install curl
+apt install wget
+apt install tor
+apt install proxychains
+apt install macchanger
+sudo apt-get -y install oracle-java8-installer
+sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr
+apt install gnupg
+apt install postgresql
+sudo apt-get install build-essential libreadline-dev  libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev openjdk-7-jre subversion git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev ruby1.9.3
+chmod 755 msfinstall
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+apt install metasploit-framework
