38ecf0f73832728f1a1feecf8a3c07042ba558cb
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-04-10 15:55:18 +0200

    Create fatrat.sh

---
+++ fatrat.sh
@@ -0,0 +1,39 @@
+
+sudo apt-get install xterm
+
+
+
+apt install mingw-w64 mono-complete
+
+
+git clone https://github.com/offensive-security/exploit-database.git
+
+
+sudo apt install software-properties-common
+
+sudo apt update && apt -y install exploitdb
+
+sudo apt install build-essential
+
+sudo apt-get install manpages-dev
+
+sudo apt install gnome-terminal
+
+sudo apt-get install -y upx
+
+apt install rbenv
+sudo apt-get install monodevelop
+
+sudo apt install apt-transport-https
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+
+sudo apt-get install -y backdoor-factory
+
+sudo apt update
+
+sudo apt install default-jre
+
+sudo apt-get install zipalign
